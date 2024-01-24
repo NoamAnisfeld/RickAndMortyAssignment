7039c5e21b9abca835b009c051797ecd27d02d37
Noam Anisfeld <NoamAnisfeld@gmail.com>
2024-01-24 23:51:28 +0200

    redesigning the main episodes page

--- src/components/episodes/AllEpisodesPage.tsx
+++ src/components/episodes/AllEpisodesPage.tsx
@@ -2,7 +2,25 @@ import { useEffect, useState } from 'react'
 import { getAllEpisodes } from '../../requests/requests'
 import type { Episode } from '../../requests/schemas'
 import { Link } from 'react-router-dom';
+import './AllEpisodesPage.css'
 
+
+function groupEpisodesIntoSeasons(episodes: Episode[]): Record<string, Episode[]> {
+
+    const results: Record<string, Episode[]> = {};
+
+    for (const episode of episodes) {
+        const season = episode.episode.match(/S0*(\d+)E/)?.[1] || '';
+
+        if (Object.hasOwn(results, season)) {
+            results[season].push(episode);
+        } else {
+            results[season] = [episode];
+        }
+    }
+
+    return results;
+}
 
 export default function AllEpisodesPage() {
 
@@ -18,16 +36,24 @@ export default function AllEpisodesPage() {
         return <>loading...</>
     }
 
+    const groupedEpisodes = groupEpisodesIntoSeasons(episodes);
+
     return (<>
         <h1>All Rick and Morty Episodes</h1>
-        <ol className="episodes">
-            {episodes.map(episode =>
-                <li>
-                    <Link to={String(episode.id)}>
-                        {episode.name}
-                    </Link>
-                </li>
+        <main className="all-episodes-container">
+
+            {Object.entries(groupedEpisodes).map(([season, episodeList]) =>
+                <section key={season} className="season-container">
+                    <h2>Season {season}</h2>
+                    <ol className="episode-list">
+                        {episodeList.map(episode =>
+                            <li key={episode.name}>
+                                <Link to={`/episodes/${episode.id}`} className="text-link">{episode.name}</Link>
+                            </li>
+                        )}
+                    </ol>
+                </section>
             )}
-        </ol>
+        </main>
     </>)
 }
